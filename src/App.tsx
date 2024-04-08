15c813118a8ba3ddfa3a0824512a3e763acf3546
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-04-08 22:49:13 +0300

    init: project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Form from "./components/form"
+import Info from "./components/info"
+
+const App = () => {
+  return (
+    <main className="px-10 py-10 flex flex-col gap-y-5">
+      <Form />
+      <Info />
+    </main>
+  )
+}
+
+export default App
