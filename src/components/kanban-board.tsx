cebf621bb37c77b3914eda26847de1ad730e2bb4
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-04-09 22:29:47 +0300

    fix: kanban board

--- src/components/kanban-board.tsx
+++ src/components/kanban-board.tsx
@@ -1,21 +1,48 @@
-import { useAppSelector } from "../redux/hooks"
+import { useState } from "react"
+import { useAppSelector, useAppDispatch } from "../redux/hooks"
+
+import {
+  DndContext,
+  DragOverlay,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core"
+import { createPortal } from "react-dom"
+
+import { updateIssuePosition } from "../redux/features/repo/repoSlice"
 
 import {
   selectRepoIssues,
   selectRepoLoading,
 } from "../redux/features/repo/repoSelectors"
 
+import { TailSpin } from "react-loader-spinner"
+import KanbanColumn from "./kanban-column"
+import KanbanItem from "./kanban-item"
+
 import { COLUMNS } from "../constants"
 
-import KanbanColumn from "./kanban-column"
-
-import { TailSpin } from "react-loader-spinner"
+import type { IColumn, IIssue } from "../types"
+import type { DragStartEvent, DragEndEvent } from "@dnd-kit/core"
+import { arrayMove } from "@dnd-kit/sortable"
 
 const KanbanBoard = () => {
+  const dispatch = useAppDispatch()
+
   const isLoading = useAppSelector(selectRepoLoading)
   const issues = useAppSelector(selectRepoIssues)
 
-  console.log(issues)
+  const [activeColumn, setActiveColumn] = useState<IColumn | null>(null)
+  const [activeIssue, setActiveIssue] = useState<IIssue | null>(null)
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 10,
+      },
+    }),
+  )
 
   if (isLoading) {
     return (
@@ -31,16 +58,71 @@ const KanbanBoard = () => {
   }
 
   return (
-    <div className="w-full flex items-center justify-center">
-      <ul className="flex gap-x-20">
-        {COLUMNS.map(column => (
-          <li key={column.id}>
-            <KanbanColumn items={issues[column.id]} column={column} />
-          </li>
-        ))}
-      </ul>
+    <div className="m-auto flex w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
+      <DndContext
+        sensors={sensors}
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
+      >
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
+            {COLUMNS.map(col => (
+              <KanbanColumn key={col.id} column={col} items={issues[col.id]} />
+            ))}
+          </div>
+        </div>
+
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <KanbanColumn
+                column={activeColumn}
+                items={issues[activeColumn.id]}
+              />
+            )}
+            {activeIssue && <KanbanItem item={activeIssue} />}
+          </DragOverlay>,
+          document.body,
+        )}
+      </DndContext>
     </div>
   )
+
+  function onDragStart(event: DragStartEvent) {
+    if (event.active.data.current?.type === "Column") {
+      setActiveColumn(event.active.data.current.column)
+      return
+    }
+
+    if (event.active.data.current?.type === "Issue") {
+      setActiveIssue(event.active.data.current.issue)
+      return
+    }
+  }
+
+  function onDragEnd(event: DragEndEvent) {
+    const { active, over } = event
+    if (!over) return
+
+    const currentIssue: IIssue = active.data.current?.issue
+
+    if (!currentIssue) return
+
+    const { columnState } = currentIssue
+
+    if (active.id !== over.id) {
+      const oldIndex = issues[columnState].findIndex(
+        item => item.id === active.id,
+      )
+      const newIndex = issues[columnState].findIndex(
+        item => item.id === over.id,
+      )
+
+      const newIssues = arrayMove(issues[columnState], oldIndex, newIndex)
+
+      dispatch(updateIssuePosition({ column: columnState, newIssues }))
+    }
+  }
 }
 
 export default KanbanBoard
--- src/components/kanban-column.tsx
+++ src/components/kanban-column.tsx
@@ -1,22 +1,10 @@
 import { useMemo } from "react"
-import { useAppDispatch } from "../redux/hooks"
 
-import { SortableContext, arrayMove } from "@dnd-kit/sortable"
-import {
-  DndContext,
-  closestCenter,
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors,
-} from "@dnd-kit/core"
-
-import { sortableKeyboardCoordinates } from "@dnd-kit/sortable"
+import { SortableContext, useSortable } from "@dnd-kit/sortable"
 
 import KanbanItem from "./kanban-item"
 
 import { type IIssue, type IColumn } from "../types"
-import { updateIssuePosition } from "../redux/features/repo/repoSlice"
 
 interface IKanbanColumnProps {
   column: IColumn
@@ -24,53 +12,36 @@ interface IKanbanColumnProps {
 }
 
 const KanbanColumn = ({ column, items }: IKanbanColumnProps) => {
-  const dispatch = useAppDispatch()
-
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    }),
-  )
-
   const issuesIds = useMemo(() => {
     return items.map(item => item.id)
   }, [items])
 
+  const { setNodeRef } = useSortable({
+    id: column.id,
+    data: {
+      type: "Column",
+      column,
+    },
+  })
+
   return (
-    <DndContext
-      sensors={sensors}
-      collisionDetection={closestCenter}
-      onDragEnd={onDragEnd}
+    <div
+      ref={setNodeRef}
+      className="w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col"
     >
-      <div className="w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
-        <div className="text-md h-[60px] cursor-grab rounded-md rounded-b-none p-3 font-bold border-columnBackgroundColor border-4 flex items-center justify-between">
-          <div className="flex gap-2">{column.title}</div>
-        </div>
-
-        <div className="flex flex-grow flex-col gap-4 p-2 overflow-x-hidden overflow-y-auto">
-          <SortableContext items={issuesIds}>
-            {items.map(item => (
-              <KanbanItem key={item.id} item={item} />
-            ))}
-          </SortableContext>
-        </div>
+      <div className="text-md h-[60px] rounded-md rounded-b-none p-3 font-bold border-columnBackgroundColor border-4 flex items-center justify-center">
+        <p className="outline-none text-center">{column.title}</p>
       </div>
-    </DndContext>
+
+      <div className="flex flex-grow flex-col gap-4 p-2 overflow-x-hidden overflow-y-auto">
+        <SortableContext items={issuesIds}>
+          {items.map(issue => (
+            <KanbanItem key={issue.id} item={issue} />
+          ))}
+        </SortableContext>
+      </div>
+    </div>
   )
-
-  function onDragEnd(event: any) {
-    const { over, active } = event
-
-    if (active.id !== over.id) {
-      const oldIndex = items.findIndex(item => item.id === active.id)
-      const newIndex = items.findIndex(item => item.id === over.id)
-
-      const newIssues = arrayMove(items, oldIndex, newIndex)
-
-      dispatch(updateIssuePosition({ column: column.id, newIssues }))
-    }
-  }
 }
 
 export default KanbanColumn
